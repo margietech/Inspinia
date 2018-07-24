e4d9ff072cdbe612db4bb3b154bbeb54913f7a84
margietech <41622222+margietech@users.noreply.github.com>
2018-07-24 17:01:10 +0530

    language

---
+++ language/english/page_label_lang.php
@@ -0,0 +1,108 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+//page heading label
+$lang['share_file_header'] = 'Share File';
+$lang['leave_management_header'] = 'Leave Management';
+$lang['user_management_header'] = 'User Management';
+$lang['leave_requests_header'] = 'Leave Requests';
+$lang['dashboard_header'] = 'Dashboard';
+$lang['profile_header'] = 'Profile';
+$lang['daily_report_header'] = 'My Daily Report';
+$lang['emp_report_header'] = 'Employee Daily Report';
+$lang['music_header'] = 'Music';
+//button label
+$lang['close_btn'] = 'Close';
+$lang['cancel_btn'] = 'Cancel';
+$lang['approve_btn'] = 'Approve';
+$lang['reject_btn'] = 'Reject';
+$lang['add_user_btn'] = 'Add User';
+$lang['upload_btn'] = 'Upload';
+$lang['download_btn'] = 'Download';
+$lang['delete_btn'] = 'Delete';
+$lang['edit_btn'] = 'Edit';
+$lang['update_btn'] = 'Update';
+$lang['submit_btn'] = 'Submit';
+$lang['request_btn'] = 'Request';
+$lang['request_leave_btn'] = 'Request Leave';
+$lang['edit_profile_btn'] = 'Edit profile';
+$lang['addmore_btn'] = 'Add More';
+//label-share file
+$lang['file_colm'] = 'File';
+$lang['size_colm'] = 'Size';
+$lang['time_colm'] = 'Uploaded Time';
+$lang['action_colm'] = 'Action';
+$lang['Select_file_label'] = 'Select file';
+$lang['file_upload_tbl'] = 'Media Master';
+//label-user management
+$lang['name_lbl'] = 'Name';
+$lang['email_lbl'] = 'Email';
+$lang['username_lbl'] = 'Username';
+$lang['pwd_lbl'] = 'Password';
+$lang['status_lbl'] = 'Status';
+$lang['fname_lbl'] = 'Firstname';
+$lang['lname_lbl'] = 'Lastname';
+$lang['active_lbl'] = 'Active';
+$lang['inactive_lbl'] = 'Inactive';
+$lang['user_master_tbl'] = 'User master';
+$lang['add_user_lbl'] = 'Add New User';
+//label-leave
+$lang['my_leave_request_tbl'] = 'My Leave requests';
+$lang['sub_lbl'] = 'Subject';
+$lang['date_lbl'] = 'Date';
+$lang['leavetype_lbl'] = 'Leave Type';
+$lang['desc_lbl'] = 'Description';
+$lang['cl_lbl'] = 'CL';
+$lang['sl_lbl'] = 'SL';
+$lang['add_request_lbl'] = 'Add Request';
+//label-leave management
+$lang['emp_leave_request_tbl'] = 'Employee Leave requests';
+$lang['approved_lbl'] = 'Approved';
+$lang['rejected_lbl'] = 'Rejected';
+$lang['view_details_lbl'] = 'View Details';
+$lang['details_lbl'] ='Detail';
+$lang['leave_id_lbl'] = 'Leave id';
+$lang['leave_detail_lbl'] = 'Leave Detail';
+//label-profile
+$lang['edit_profile_lbl'] = 'Edit Profile';
+$lang['city_lbl'] = 'City';
+$lang['designation_lbl'] = 'Designation';
+$lang['country_lbl'] = 'Country';
+//label-daily report
+$lang['add_report_lbl'] = 'Add new report';
+$lang['task_lbl'] = 'Task';
+$lang['generalissue_lbl'] = 'General issues';
+$lang['issue_lbl'] = 'Issue';
+$lang['duration_lbl'] = 'Duration';
+$lang['my_daily_report_tbl'] = 'My Daily Report';
+$lang['edit_task_lbl'] = 'Edit Task';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+
+//label-employee report
+$lang['emp_name_lbl'] = 'Employee name';
+$lang['report_detail_lbl'] = 'Report Detail';
+$lang['report_id_lbl'] = 'Report id';
+$lang['emp_report_tbl'] = 'Employee Reports';
+//label-music
+$lang['audio_tbl'] = 'Audios';
+$lang['song_lbl'] = 'Song';
+$lang['jazz_lbl'] = 'Jazz';
+$lang['pop_lbl'] = 'Pop';
+$lang['classical_lbl'] = 'Classical';
+$lang['hiphop_lbl'] = 'Hiphop';
+$lang['opera_lbl'] = 'Opera';
+$lang['instrumental_lbl'] = 'Instrumental';
+$lang['genre_lbl'] = 'Genre';
+$lang['folk_music_lbl'] = 'Folk music';
+$lang['title_lbl'] = 'Title';
+$lang['artist_lbl'] = 'Artist';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+$lang[''] = '';
+
+?>
---
+++ language/english/template_label_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+$lang['Logout'] = 'Log out';
+$lang['Profile'] = 'Profile';
+$lang['Contacts'] = 'Contacts';
+$lang['Mailbox'] = 'Mailbox';
+$lang['Dashboard'] = 'Dashboard';
+$lang['Share'] = 'Share';
+$lang['User Management'] = 'User Management';
+$lang['Leave'] = 'Leave';
+$lang['Employee Leave Requests'] = 'Employee Leave Requests';
+$lang['Daily Report'] = 'Daily Report';
+$lang['Employee Report'] = 'Employee Report';
+$lang['Music'] = 'Music';
+//$lang[''] = '';
+?>
